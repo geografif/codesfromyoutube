79df10e1d69b4d74642d3cebf737c40cf4c3964e
M. Afif Fauzan <48111760+geografif@users.noreply.github.com>
2023-05-04 10:58:24 +0200

    Create Global Industrial and Smallholder Oil Palm Plantation Map from BIOPAMA.js

---
+++ Open Geospatial Data/Global Industrial and Smallholder Oil Palm Plantation Map from BIOPAMA.js
@@ -0,0 +1,21 @@
+// Global Industrial and Smallholder Oil Palm Plantation Map from BIOPAMA
+// by Geografif
+// Run on Google Earth Engine: https://destyy.com/egvTGR
+
+// Define dataset
+var biopama19 = ee.ImageCollection("BIOPAMA/GlobalOilPalm/v1").select('classification').mosaic();
+var maskOP = biopama19.neq(3).where(biopama19.eq(0), 0.6);
+var biopama19 = biopama19.updateMask(maskOP).clip(hotspot)
+
+// Display
+Map.centerObject(hotspot, 9);
+Map.setOptions('SATELLITE');
+Map.addLayer(biopama19, {min:1,max:3,palette:['ff0000','ff0111']}, 'Oil Palm');
+
+// Export
+Export.image.toDrive({
+  image: biopama19,
+  description: 'op',
+  region: hotspot,
+  scale: 10
+});
